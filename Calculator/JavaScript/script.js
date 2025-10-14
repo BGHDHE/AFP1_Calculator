1ba1d2b2ab166d4d275624907bb04f594ca905d5
Rantott <benedekkerekes@gmail.com>
2025-10-14 15:11:45 +0200

    Gomblenyomás "stílusa" megváltoztatva + js kezdete

---
+++ Calculator/JavaScript/script.js
@@ -0,0 +1,10 @@
+let buttonInput = document.querySelectorAll(".button");
+let input = document.getElementById("display");
+let equal = document.getElementById("equal");
+let clear = document.getElementById("clear");
+let erase = document.getElementById("erase");
+let historyContent = document.getElementById("historyContent");
+
+window.onload = () => {
+    input.value = "";
+};
